55cdd6219bea9c31b7715ac0b10a5c70a1d2601d
Student <example@example.com>
2019-11-20 11:30:08 -0800

    use async function and await keyword.

--- src/weather-interface.js
+++ src/weather-interface.js
@@ -3,20 +3,21 @@ import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import './styles.css';
 // import img from './file.png'; <-- file-loader
+
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
     const city = $('#location').val();
     $('#location').val("");
 
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`)
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(jsonifiedResponse) {
-        getElements(jsonifiedResponse);
-      });
+    asyncApiCall();
 
-   const getElements = function(response) {
+    async function asyncApiCall() {
+      let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`);
+      let jsonifiedResponse = await response.json();
+      getElements(jsonifiedResponse);
+    }
+
+    const getElements = function(response) {
       $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%.`);
       $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
     };
