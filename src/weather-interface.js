148732b849bac19f31491b9a522bfc021705103f
Student <example@example.com>
2019-11-20 11:38:10 -0800

    add comments to denote separation of business logic and UI logic.

--- src/weather-interface.js
+++ src/weather-interface.js
@@ -10,8 +10,10 @@ $(document).ready(function() {
     $('#location').val("");
 
     (async () => {
+      // The API call is business logic.
       let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`);
       let jsonifiedResponse = await response.json();
+      // The getElements method is UI logic so it will need to be separated from the business logic.
       getElements(jsonifiedResponse);
     })();
 
